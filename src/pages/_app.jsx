e011f2b527f93973ef3cad0d6f4dbc0e1950b6dc
Andrew Barrows <abarrows@amuniversal.com>
2022-03-14 15:21:25 +0000

    Updating the app.jsx with some basic scaffold.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,49 @@
-import '../style/index.css'
+import '../style/index.css';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { DefaultSeo } from 'next-seo';
 
-export default function AmuProduct({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+
+const AmuProduct = ({ Component, pageProps }) => (
+    <>
+      {/* There should only be one Head Component, see https://nextjs.org/docs/api-reference/next/head */}
+      <DefaultSeo {...SEO} />
+      <Head>
+        <link
+          href="/images/favicons/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/images/favicons/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/images/favicons/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link href="/favicon-manifest.json" rel="manifest" />
+        <link
+          color="#05354e"
+          href="/images/favicons/safari-pinned-tab.svg"
+          rel="mask-icon"
+        />
+        <meta content="#05354e" name="msapplication-TileColor" />
+        <meta content="#ffffff" name="theme-color" />
+        <script src="https://utilities.amuniversal.com/unsupportedbrowsers/index.js" />
+      </Head>
+    </>
+  );
+
+AmuProduct.propTypes = {
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
+export default AmuProduct;
+
