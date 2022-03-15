5a39850f348e9d62c704e8081ae567ca0e1d5748
Andrew Barrows <abarrows@amuniversal.com>
2022-03-15 16:12:47 +0000

    Removing a few packages from package.json, cleaning up the app.jsx and adding ErrorBoundary and error page.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,52 +1,77 @@
-import '../style/index.css';
+/* eslint-disable react/jsx-props-no-spreading, react/forbid-prop-types */
+// Core
+import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+
+// Components
 // import { DefaultSeo } from 'next-seo';
 // import SEO from 'next-seo.config';
 
+// Helpers
+import ErrorBoundary from 'src/components/commons/ErrorBoundary';
 
-const AmuProduct = ({ Component, pageProps }) => (
-    <>
-      {/* There should only be one Head Component, see https://nextjs.org/docs/api-reference/next/head <DefaultSeo {...SEO} /> */}
+// Styles
+import '../style/index.scss';
+
+const AmuProduct = ({ Component, err, pageProps }) => {
+
+  const Template = Component.template;
+  return (
+  <>
+      {/* There should only be one Head Component, see https://nextjs.org/docs/api-reference/next/head */}
+      {/* <DefaultSeo {...SEO} /> */}
       <Head>
-        {/* <link
-          href="/images/favicons/apple-touch-icon.png"
-          rel="apple-touch-icon"
-          sizes="180x180"
-        />
-        <link
-          href="/images/favicons/favicon-32x32.png"
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          href="/images/favicons/favicon-16x16.png"
-          rel="icon"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link href="/favicon-manifest.json" rel="manifest" />
-        <link
-          color="#05354e"
-          href="/images/favicons/safari-pinned-tab.svg"
-          rel="mask-icon"
-        /> */}
-        <meta content="#05354e" name="msapplication-TileColor" />
-        <meta content="#ffffff" name="theme-color" />
-        <script src="https://utilities.amuniversal.com/unsupportedbrowsers/index.js" />
+      {/* <link
+        href="/images/favicons/apple-touch-icon.png"
+        rel="apple-touch-icon"
+        sizes="180x180"
+      />
+      <link
+        href="/images/favicons/favicon-32x32.png"
+        rel="icon"
+        sizes="32x32"
+        type="image/png"
+      />
+      <link
+        href="/images/favicons/favicon-16x16.png"
+        rel="icon"
+        sizes="16x16"
+        type="image/png"
+      />
+      <link href="/favicon-manifest.json" rel="manifest" />
+      <link
+        color="#05354e"
+        href="/images/favicons/safari-pinned-tab.svg"
+        rel="mask-icon"
+      /> */}
+      <meta content="#05354e" name="msapplication-TileColor" />
+      <meta content="#ffffff" name="theme-color" />
+      <script src="https://utilities.amuniversal.com/unsupportedbrowsers/index.js" />
     </Head>
-    <h1>
-      Welcome to an <a href="/" id="onboarding">AmuProduct!</a>
-    </h1>
-    <p>This is a test paragraph for the homepage.</p>
-    </>
+    <ErrorBoundary>
+      <>
+        {Template ? (
+          <Template {...pageProps}>
+            <Component {...pageProps} err={err} />
+          </Template>
+        ) : (
+          <Component {...pageProps} err={err} />
+        )}
+      </>
+    </ErrorBoundary>
+  </>
   );
+};
 
 AmuProduct.propTypes = {
   Component: PropTypes.func.isRequired,
+  err: PropTypes.any,
   pageProps: PropTypes.object.isRequired,
 };
 
-export default AmuProduct;
+AmuProduct.defaultProps = {
+  err: undefined,
+};
 
+export default AmuProduct;
