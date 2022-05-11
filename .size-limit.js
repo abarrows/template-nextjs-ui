70516a8908193ad0a97c532b0fdfd4d75c9c8c4c
ctrble <ctrble@gmail.com>
2022-05-11 14:15:17 -0500

    updating size limit action, ran build which included the sitemap

--- .size-limit.js
+++ .size-limit.js
@@ -4,26 +4,31 @@ const zlib = require('zlib');
 
 // Get all pages and their chunks
 const bundle = require('./.next/build-manifest.json');
-const pageNames = Object.keys(bundle.pages);
+const pageNames = Object.keys(bundle.pages).filter(
+  (name) => name !== '/_app' && name !== '/_error' && !name.endsWith('.xml')
+);
+
+// Separate shared dependencies
+const sharedDependencies = bundle.pages['/_app'];
 
 // Build a list of dependencies for each page for size-limit to evaluate
 const pages = pageNames.map((name) => {
-  const pageChunks = bundle.pages[name].map(
-    (dependency) => `.next/${dependency}`
+  const uniqueChunks = bundle.pages[name].filter(
+    (dependency) => !sharedDependencies.includes(dependency)
   );
 
   return {
     name,
     limit: '500 kb',
-    path: pageChunks,
+    path: uniqueChunks.map((chunk) => `.next/${chunk}`),
   };
 });
 
 module.exports = [
   {
-    name: 'Global Critical CSS (included in page calculations)',
-    limit: '100 kb',
-    path: '.next/static/css/*.css',
+    name: 'Shared by All',
+    limit: '200 kb',
+    path: sharedDependencies.map((dependency) => `.next/${dependency}`),
   },
   ...pages,
 ];
