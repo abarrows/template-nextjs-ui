31b3938976166995f654a6c2914861e810e6134f
ctrble <ctrble@gmail.com>
2023-05-30 16:33:29 -0500

    add playwright

---
+++ playwright/fixtures/pages/Homepage.spec.js
@@ -0,0 +1,12 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Homepage', () => {
+  test('displays a reference to page.jsx and a link to the README', async ({
+    page,
+  }) => {
+    await page.goto('/');
+
+    await expect(page.getByText(/src\/app\/page.jsx/)).toBeVisible();
+    await expect(page.getByRole('link', { name: /readme/i })).toBeVisible();
+  });
+});
