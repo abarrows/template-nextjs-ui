be9685b60dee0ed9d6ffe95c3d3cf7bece2f56b6
Andrew Barrows <abarrows@amuniversal.com>
2023-11-29 12:36:15 -0600

    [ab-2023] Updating the next.config to be typescript supported, improved console output, and updated the deprecated images: domains: object needed in next.js 14.

--- next.config.js
+++ next.config.js
@@ -1,50 +1,77 @@
-// Add timestamps to console logs
-require('console-stamp')(console);
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Importing sentry configuration
 
-// Optionally analyze client, server, and edge/middleware bundle sizes,
-// Run with `yarn build:analyze`
+// Importing package.json for app level values.
+const consoleFormat = require('console-stamp');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const packageJson = require('./package.json');
+// Reformat console logs to be cleaner and more readable
+consoleFormat(console, {
+  pattern: 'HH:MM:ss.l',
+  colors: {
+    stamp: 'yellow',
+    label: 'white',
+    metadata: 'green',
+  },
+  label: false,
+  metadata: `[${packageJson.name}]`,
+});
+// Optionally analyze client, server, and edge/middleware bundle sizes,
+// Run with `yarn build:analyze`
 
 // Permanent site redirects file, see that for adding new redirects
 const redirects = require('./redirects');
 
-const customConfig = {
-  eslint: {
+// eslint-disable-next-line require-await
+module.exports = async () => {
+  const nextConfig = {
+    /* eslint-disable-next-line require-await */
+    async redirects() {
+      const prodRedirects = [];
+      if (process.env.NEXT_PUBLIC_DEPLOY_ENV === 'production') {
+        // prodRedirects.push({
+        //   source: 'url to disable',
+        //   destination: '/',
+        //   permanent: false,
+        // });
+      }
+      return [...prodRedirects, ...redirects];
+    },
+    // The eslint ignoreDuringBuilds boolean allows production builds to successfully complete even if
+    // the project has ESLint errors. This is set to true because we already have ESLint configured to
+    // run in a separate workflow
+    eslint: { ignoreDuringBuilds: true },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cmsassets.gocomics.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'avatar.amuniversal.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'contentassets.amuniversal.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+    productionBrowserSourceMaps: true,
+    swcMinify: true,
     // The eslint ignoreDuringBuilds boolean allows production builds to
     // successfully complete even if the project has ESLint errors. This is
     // set to true because we already have ESLint configured to run in a
     // separate workflow.
     ignoreDuringBuilds: true,
-  },
-  images: {
-    // Breakpoints, plus 2x 1200 and 1400 for retina screens
-    deviceSizes: [576, 768, 992, 1200, 1400, 2400, 2800],
-    // TODO: ONBOARDING - Set the correct CDN host
-    domains: ['cmsassets.amuproduct.com'],
-    // TODO: allow avif once the Storybook addon supports it
-    // https://github.com/RyanClementsHax/storybook-addon-next#avif
-    // formats: ['image/avif', 'image/webp'],
-  },
-  productionBrowserSourceMaps: true,
-  swcMinify: true,
-
-  async redirects() {
-    // Production-only redirects
-    // These are used as a quick feature flag when certain pages should be
-    // temporarily excluded from the prod site.
-    const prodRedirects = [];
-    if (process.env.NEXT_PUBLIC_DEPLOY_ENV === 'production') {
-      // prodRedirects.push({
-      //   source: 'url to disable',
-      //   destination: '/',
-      //   permanent: false,
-      // });
-    }
-
-    return [...prodRedirects, ...redirects];
-  },
+  };
+  return withBundleAnalyzer(nextConfig);
 };
-
-module.exports = withBundleAnalyzer(customConfig);
