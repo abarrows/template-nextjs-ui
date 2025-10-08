a83f1318c8521cb151f1dc83d1a94a976dbdd184
Andy Barrows <andyb@retailsuccess.com>
2025-10-08 17:03:00 -0500

    refactor: clean up layout code and fix formatting in config files

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,33 +1,19 @@
 import React from 'react';
 
-import classNames from 'classnames';
 // eslint-disable-next-line camelcase
-// Remove Carter One import and usage
-// import { Carter_One, Source_Code_Pro } from 'next/font/google';
-// const carterOne = Carter_One({ ... });
 import { Source_Code_Pro } from 'next/font/google';
+import PropTypes from 'prop-types';
+
+import ShowSiteHeader from 'src/components/sections/ShowSiteHeader';
+
+import 'src/styles/index.scss';
+
 const sourceCodePro = Source_Code_Pro({
   display: 'swap',
   subsets: ['latin'],
   variable: '--font-source-code-pro',
   weight: ['400', '700'],
 });
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang='en'>
-      <body className={sourceCodePro.variable}>
-        <ShowSiteHeader />
-        {children}
-      </body>
-    </html>
-  );
-}
-import PropTypes from 'prop-types';
-
-import ShowSiteHeader from 'src/components/sections/ShowSiteHeader';
-
-import 'src/styles/index.scss';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
@@ -36,32 +22,16 @@ export const metadata = {
   description: 'Starter kit for new UI apps',
 };
 
-// const carterOne = Carter_One({
-//   display: 'swap',
-//   subsets: ['latin'],
-//   variable: '--font-carter-one',
-//   weight: '400',
-// });
-const sourceCodePro = Source_Code_Pro({
-  display: 'swap',
-  subsets: ['latin'],
-  variable: '--font-source-code-pro',
-  weight: ['400', '700'],
-});
-
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      {/* <body className={classNames(carterOne.variable, sourceCodePro.variable)}> */}
       <body className={sourceCodePro.variable}>
         <ShowSiteHeader />
-
         {children}
       </body>
     </html>
   );
 }
-
 RootLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
